1d826599b4ea5b925053cfe3d6d50c96c827158e
git <martinmenalled@gmail.com>
2022-01-14 12:56:00 -0300

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AccesoperfilService } from 'src/app/services/accesoperfil.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+datos: any;
+  constructor(private miservicio:AccesoperfilService) { }
 
   ngOnInit(): void {
+    this.miservicio.getDatosPerfil().subscribe(data =>{
+      this.datos = data["perfil"]
+    })
   }
 
 }
